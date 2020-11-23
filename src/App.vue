4f93c43186d6f636eaad81b86648cf93c3aab4ab
Fodro <feodor.tomilov@yandex.ru>
2020-11-23 22:37:56 +0500

    Адаптивная вёрстка

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,47 @@ export default {
 </script>
 
 <style>
-#app {
-  color: #2c3e50;
+@media not handheld {
+  #app {
+    color: #2c3e50;
+  }
+
+  .JinguLogo {
+    display: inline-block;
+    text-align: end;
+    margin-left: 50px;
+    padding: 0;
+    margin-bottom: 0;
+  }
+
+  .NameText {
+    display: inline-block;
+    padding: 0;
+    margin-bottom: 0;
+    margin-left: 10px;
+  }
+
+  .Emoji {
+    padding: 0;
+    margin-top: 0;
+    margin-left: 0px;
+  }
 }
-.JinguLogo {
-  display: inline-block;
-  text-align: end;
-  margin-left: 50px;
-  padding: 0;
-  margin-bottom: 0;
-}
-.NameText {
-  display: inline-block;
-  padding: 0;
-  margin-bottom: 0;
-  margin-left: 10px;
-}
-.Emoji {
-  padding: 0;
-  margin-top: 0;
-  margin-left: 0px;
+@media screen and (max-device-width: 1080px) and (max-device-height: 2400px){
+  #app {
+    color: #2c3e50;
+  }
+
+  .JinguLogo {
+    display: flex;
+  }
+
+  .NameText {
+    display: flex;
+  }
+
+  .Emoji {
+    display: flex;
+  }
 }
 </style>
